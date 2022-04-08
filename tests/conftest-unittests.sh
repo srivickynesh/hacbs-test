6cba0611c434dafb84ea9ca37d0b6556e7aa6c47
Sri Vignesh <sselvan@redhat.com>
2022-04-08 19:12:19 +0530

    Update conftest-unittests.sh

--- tests/conftest-unittests.sh
+++ tests/conftest-unittests.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bats
 
 # create temporary file
-tmpfile=$(mktemp)
-
 if [ -z "$1" ]
 then
   IMAGE=("quay.io/cvpops/test-index:v4.10")
 else
   IMAGE=$1
 fi
+if [ -z "$2" ]
+then
+  jsonfile="../../image_metadata.json"
+else
+  jsonfile=$2
+  echo "Filename: $jsonfile"
+fi
+
 
 # run "podman run --rm quay.io/skopeo/stable inspect docker://${IMAGE}" >> ${tmpfile}
 #run "docker inspect ${IMAGE}" >> ${tmpfile}
@@ -17,14 +23,14 @@ fi
 
 cat "$tmpfile"
 
-if [[ -s $tmpfile ]] ; then
+if [[ -s $jsonfile ]] ; then
 echo "$FILE has json image data."
 else
 echo "$FILE is empty."
 fi ;
 
 @test "policies/image/deprecated-images" {
-  run "conftest test --policy policies/image/policy/deprecated-image.rego image_metadata.json --output=json"
+  run "conftest test --policy policies/image/policy/deprecated-image.rego ${jsonfile} --output=json"
   [ "$status" -eq 0 ]
 }
 rm "$tmpfile"
