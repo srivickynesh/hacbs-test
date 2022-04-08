cdc5da51f1190508a6dc283129f6d69a6c17fedd
Sri Vignesh <sselvan@redhat.com>
2022-04-08 16:55:23 +0530

    Create conftest-unittests.sh

---
+++ tests/conftest-unittests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bats
+
+load bats-support-clone
+load test_helper/bats-support/load
+load test_helper/redhatcop-bats-library/load
+
+# create temporary file
+tmpfile=$(mktemp)
+
+if [ -z "$1" ]
+then
+  IMAGE=$("quay.io/cvpops/test-index:v4.10")
+else
+  IMAGE=$1
+fi
+
+echo "Using image : "$IMAGE 
+
+skopeo inspect docker://${IMAGE} > ${tmpfile}
+
+@test "policies/image/deprecated-images" {
+  cmd="conftest test --policy image/policy/ ${tmpfile} --output=json"
+  run ${cmd}
+}
