9fe32b0456adc7b6e7703ac115d0ed283ae86e46
Sri Vignesh <sselvan@redhat.com>
2022-04-08 18:13:00 +0530

    Update conftest-unittests.sh

--- tests/conftest-unittests.sh
+++ tests/conftest-unittests.sh
@@ -12,7 +12,8 @@ fi
 
 run "podman run --rm quay.io/skopeo/stable inspect docker://${IMAGE} > ${tmpfile}"
 
+echo "$tmpfile"
+
 @test "policies/image/deprecated-images" {
-  cmd="conftest test --policy policies/image/policy/deprecated-image.rego ${tmpfile} --output=json"
-  run ${cmd}
+  run "conftest test --policy policies/image/policy/deprecated-image.rego ${tmpfile} --output=json"
 }
